1d1f273163686f6242c44030d22bea9cfbdfb2be
nuseybekarakurt-bee <nuseybe.karakurt@designbees.ch>
2025-02-04 22:33:22 +0100

    Depatment has been created

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use App\Enums\RolesEnum;
+use Illuminate\Database\Eloquent\Model;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -59,5 +60,8 @@ class AdminPanelProvider extends PanelProvider
             //     Authenticate::class,
             // ])
             ;
+    }
+    public function boot(){
+        Model::unguard();
     }
 }
