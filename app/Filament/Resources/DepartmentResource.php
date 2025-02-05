742b68ff562ce3e06c43e226c0236a5d5711f33c
nuseybekarakurt-bee <nuseybe.karakurt@designbees.ch>
2025-02-05 22:05:14 +0100

    implement product CRUD

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -73,7 +73,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            RelationManagers\CategoriesRelationManager::class
+            //
         ];
     }
 
